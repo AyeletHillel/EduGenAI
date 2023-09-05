7755ebf4999db2d2f7aaec51ef3d5db8f584e0eb
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-09-06 01:13:17 +0300

    added evaluateResponse function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import readline from 'readline';
 import chatgpt from './chatgpt.js';
-import standards from './standards.json' assert { type: 'json' }; // Import the JSON file
+import standards from './standards.json' assert { type: 'json' }; 
 
 const textTypes = standards["Text Types and Purposes"];
 
@@ -13,7 +13,8 @@ const rl = readline.createInterface({
 });
 
 let studentInterestTopic = null; // Variable to store the Student Interest Topic
-
+let isAnsweringFRQ = false; // Flag to check if the user is answering the Free Response Question
+let evaluationMetrics = null; // Variable to store the evaluation metrics
 
 const generateFRQ = async (topic) => {
     const textTypeKeys = Object.keys(textTypes);
@@ -33,17 +34,31 @@ const generateFRQ = async (topic) => {
     Generate a free response question suitable for a 4th grader. The question should be based on the topic of ${topic} and the writing standard provided.
     
     Provide Context for the User:
-    Generate a brief context about ${topic} to help a 4th grader understand the basics before they answer the free response question.
+    Generate a brief context about ${topic} to help a 4th grader understand the basics before they answer the free response question.`;
+
+    const prompt2 = `
     
     Create an Evaluation Metric:
     Generate an evaluation metric suitable for evaluating a 4th grader's response to the free response question about ${topic}. The evaluation metric should align with the sub-points of the writing standard.
-    `;
+
+    Writing standard: description: ${description}, instructions: ${instructions}`;
     
-    // Send the prompt to GPT for further processing
+    evaluationMetrics = await chatgpt(prompt2); // Store the metrics for later use
     const response = await chatgpt(prompt);
     console.log(response);
     console.log("\nPlease answer the above free response question:");
-    askQuestion()
+    isAnsweringFRQ = true;
+};
+
+const evaluateResponse = async (response) => {
+    const prompt = `You are an AI-powered educational assistant designed to evaluate 4th-grade students free response questions. 
+
+    Your task is to evaluate the student's response based on the following evaluation metrics: ${evaluationMetrics}. 
+    
+    Student's answer: ${response}`;
+
+    const evaluation = await chatgpt(prompt);
+    console.log(`${evaluation}`);
 };
 
 const askQuestion = () => {
@@ -53,17 +68,26 @@ const askQuestion = () => {
             return;
         }
 
-        // If studentInterestTopic is null, this is the first input from the user
         if (!studentInterestTopic) {
             studentInterestTopic = input;
             console.log(`\nGot it! You're interested in ${studentInterestTopic}. Let's dive in!`);
-            await generateFRQ(studentInterestTopic); // Generate FRQ based on the topic
+            await generateFRQ(studentInterestTopic);
+            isAnsweringFRQ = true; // Set the flag to true after generating the FRQ
+            askQuestion(); 
+        } else if (isAnsweringFRQ) {
+            const studentResponse = input; // Store the user's response to the FRQ
+            await evaluateResponse(studentResponse); 
+            isAnsweringFRQ = false; // Reset the flag after evaluating the answer
+            console.log("\nWould you like to explore another topic or have any other questions?");
+            askQuestion(); 
         } else {
-            const evaluation = await chatgpt(input);
-            console.log(`${evaluation}`);
+            const response = await chatgpt(input);
+            console.log(`${response}`);
+            askQuestion(); 
         }
     });
 };
+
 
 console.log("Hello young scholar! 🌟 I'm EduGenAI, your friendly study buddy. My main goal is to help you study better and make learning fun! 📚✨ What topic are you interested in today? It could be anything like \"dinosaurs\", \"planets\", or \"history tales\". Just let me know, and we'll dive right in!");
 console.log("\nType 'exit' to end the conversation.");
